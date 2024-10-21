726a200817e5c937a9dfcf1bbc3d88a250ffe30e
Eduard Gabriel Mirea <eduardmirea00@gmail.com>
2024-10-21 16:15:29 +0200

    Modifical barra de navegación de las categorias para que coja las imagenes

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -2,16 +2,22 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PageController extends AbstractController
 {
     #[Route('/', name: 'index')]
-    public function index(): Response
+    public function index($doctrine,Request $request): Response
     {
-        return $this->render('index.html.twig', []);
+        $repository = $doctrine->getRepository(Category::class);
+
+        $categories = $repository->findAll();
+
+        return $this->render('index.html.twig', ['categories' => $categories]);
     }
 
 
